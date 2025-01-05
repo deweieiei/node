9c49b936a9c7e27b019a4b431da4168458420c76
karndiy <karndiy@gmail.com>
2025-01-05 16:34:49 +0700

    kup

--- app.js
+++ app.js
@@ -1,33 +1,65 @@
+// const express = require('express');
+// const app = express();
+// const cors = require('cors');
+// const bodyParser = require('body-parser');  // To handle POST body data
+
+
+// app.use(express.json()); // For parsing JSON bodies
+// app.use(cors());
+// //app.use(bodyParser.json());
+
+// const apiApp = require('./api/apiapp');
+
+// app.set('view engine', 'ejs');
+// app.set('views', './views');
+
+
+
+
+// app.get('/', (req, res) => {
+//     res.render('home', { title: 'BeingStory', message: 'Welcome' });
+// });
+// app.get('/login', (req, res) => {
+//     res.render('login', { title: 'Login', message: 'Login' });////77777
+// });
+
+// app.use('/apiapp', apiApp);
+
+
+
+// app.use(express.static('public'));
+// app.listen(80, () => {
+//     console.log('Server is running  ');
+// });
+
+
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const bodyParser = require('body-parser');  // To handle POST body data
+const bodyParser = require('body-parser');  // You can still keep bodyParser if you prefer, but it's better to use express.json()
+const path = require('path');
 
-
+// Replace body-parser with express.json() which is the newer way
 app.use(express.json()); // For parsing JSON bodies
 app.use(cors());
-//app.use(bodyParser.json());
 
 const apiApp = require('./api/apiapp');
 
 app.set('view engine', 'ejs');
-app.set('views', './views');
-
-
-
+app.set('views', path.join(__dirname, 'views'));
 
 app.get('/', (req, res) => {
     res.render('home', { title: 'BeingStory', message: 'Welcome' });
 });
+
 app.get('/login', (req, res) => {
-    res.render('login', { title: 'Login', message: 'Login' });////77777
+    res.render('login', { title: 'Login', message: 'Login' });
 });
 
-app.use('/apiapp', apiApp);
+app.use('/apiapp', apiApp); // This will route all API requests prefixed with /apiapp
 
+app.use(express.static(path.join(__dirname, 'public')));
 
-
-app.use(express.static('public'));
 app.listen(80, () => {
-    console.log('Server is running  ');
+    console.log('Server is running on http://localhost:80');
 });
