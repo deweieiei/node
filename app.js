b0a4e3195803319ddeb04d9e9377990547d4edf8
dew <babydew2015@gmail.com>
2025-01-09 20:02:33 +0700

    up

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const path = require('path');
  
 app.use(express.json());  
 app.use(cors());
+app.use(express.static(path.join(__dirname, 'public')));
 
 const apiApp = require('./api/apiapp');
 
@@ -14,7 +15,7 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.get('/', (req, res) => {
-    res.render('home', { title: 'BeingStory', message: 'Welcome' });
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.get('/login', (req, res) => {
