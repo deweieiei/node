acce020cda60747a80e3c3344ae0a0a4d721a669
dew <babydew2015@gmail.com>
2025-02-25 12:01:40 +0700

    up

--- api/apiapp.js
+++ api/apiapp.js
@@ -7,123 +7,6 @@ const fs = require("fs");
 
 const getCurrentDateTime = () => new Date().toISOString();
 
-router.get('/', (req, res) => {
-  res.json({
-    success: true,
-    message: 'API is working',
-    dateTime: getCurrentDateTime(),
-  });
-});
-
-router.get('/get-all-users', (req, res) => {
-  const query = 'SELECT * FROM `user`';
-  db.query(query, (err, results) => {
-    if (err) {
-      console.error('Error fetching users:', err.message);
-      return res.status(500).json({
-        success: false,
-        message: 'Error fetching users',
-        error: err.message,
-        dateTime: getCurrentDateTime(),
-      });
-    }
-    if (!results || results.length === 0) {
-      return res.status(404).json({
-        success: false,
-        message: 'No users found in the database',
-        dateTime: getCurrentDateTime(),
-      });
-    }
-    res.json({
-      success: true,
-      message: 'Users retrieved successfully',
-      data: results,
-      dateTime: getCurrentDateTime(),
-    });
-  });
-});
- 
-router.post('/login', (req, res) => {
-  const { username, password } = req.body;
-
-  if (!username || !password) {
-    return res.status(400).json({
-      success: false,
-      message: 'Username and password are required',
-      dateTime: getCurrentDateTime(),
-    });
-  }
-
-  const query = 'SELECT * FROM `user` WHERE `username` = ?';
-
-  db.query(query, [username], (err, results) => {
-    if (err) {
-      console.error('Error fetching user:', err.message);
-      return res.status(500).json({
-        success: false,
-        message: 'Error fetching the user',
-        error: err.message,
-        dateTime: getCurrentDateTime(),
-      });
-    }
-
-    if (results.length === 0) {
-      return res.status(404).json({
-        success: false,
-        message: 'User not found',
-        dateTime: getCurrentDateTime(),
-      });
-    }
-
-    const user = results[0];
-
-    if (user.password !== password) {
-      return res.status(401).json({
-        success: false,
-        message: 'Incorrect password',
-        dateTime: getCurrentDateTime(),
-      });
-    }
-
- 
-    delete user.password;
-
-    res.json({
-      success: true,
-      message: 'Login successful',
-      data: user,
-      dateTime: getCurrentDateTime(),
-    });
-  });
-});
-
-router.post('/addpolicy', (req, res) => {
-  const { section, content } = req.body;
-  
-  const insertUserQuery = 'INSERT INTO `privacy_policy` (`section`, `content`, `create_time`) VALUES (?, ?, ?)';
-  db.query(insertUserQuery, [section, content, getCurrentDateTime()], (err, result) => {
-    if (err) {
-      console.error('Error inserting user:', err.message);
-      return res.status(500).json({
-        success: false,
-        message: 'Error saving the Policy',
-        error: err.message,
-        dateTime: getCurrentDateTime(),
-      });
-    }
-    res.json({
-      success: true,
-      message: 'Policy saved successfully',
-      data: {
-        policyId: result.insertId,
-      },
-      dateTime: getCurrentDateTime(),
-    });
-  });
-}); 
- 
-
-
 const uploadDir = path.join(__dirname, "../userimage");
 if (!fs.existsSync(uploadDir)) {
   fs.mkdirSync(uploadDir, { recursive: true });
@@ -138,7 +21,7 @@ const storage = multer.diskStorage({
     cb(null, customFilename + path.extname(file.originalname));
   },
 });
-const upload = multer({ storage });
+const upload = multer({storage , limits: { fileSize: 10 * 1024 * 1024 },});
 
 // API ลงทะเบียน + อัปโหลดรูป
 router.post("/register", upload.single("image"), (req, res) => {
@@ -204,6 +87,61 @@ router.post("/register", upload.single("image"), (req, res) => {
     });
   });
 });
+ 
+router.post('/login', (req, res) => {
+  const { username, password } = req.body;
+
+  if (!username || !password) {
+    return res.status(400).json({
+      success: false,
+      message: 'Username and password are required',
+      dateTime: getCurrentDateTime(),
+    });
+  }
+
+  const query = 'SELECT * FROM `user` WHERE `username` = ?';
+
+  db.query(query, [username], (err, results) => {
+    if (err) {
+      console.error('Error fetching user:', err.message);
+      return res.status(500).json({
+        success: false,
+        message: 'Error fetching the user',
+        error: err.message,
+        dateTime: getCurrentDateTime(),
+      });
+    }
+
+    if (results.length === 0) {
+      return res.status(404).json({
+        success: false,
+        message: 'User not found',
+        dateTime: getCurrentDateTime(),
+      });
+    }
+
+    const user = results[0];
+
+    if (user.password !== password) {
+      return res.status(401).json({
+        success: false,
+        message: 'Incorrect password',
+        dateTime: getCurrentDateTime(),
+      });
+    }
+
+ 
+    delete user.password;
+
+    res.json({
+      success: true,
+      message: 'Login successful',
+      data: user,
+      dateTime: getCurrentDateTime(),
+    });
+  });
+});
+  
 
 module.exports = router;
 
